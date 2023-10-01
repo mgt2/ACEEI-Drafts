a4307faf35d118fc4fdc2bae476ee1490306855b
Garner Thompson <garnerthompson@dynamic-oit-ip4-wifirestricted01-10-16-165-120.princeton.edu>
2023-10-01 13:39:14 -0400

    more realistic class times

--- generate.py
+++ generate.py
@@ -17,12 +17,19 @@ def generate_etas (n, m) :
 # minl : lower limit of courses of type i that a student can take
 # l : upper limit of courses of type i that a student can take
 # t : number of types of courses
-def generate_constraints(m, starttime, endtime, minl, l, t) :
-    times = np.random.randint(starttime, endtime, size=(1, m))
-    types = np.random.randint(0, t, size=(1, m))
-    maxes = np.random.randint(minl, l, size= (1, t))
+def generate_constraints(m, class_times, minl, l, t) :
+    times = np.random.randint(0, len(class_times), size=(1, m))[0]
+    class_length = np.random.choice([0.833, 1.333, 2.833], size = m)
+    tuples = []
+    for i in range(len(times)) :
+        tuples.append((class_times[times[i]], class_times[times[i]] + class_length[i]))
+    
+    tuples = np.array(tuples)
+    types = np.random.randint(0, t, size=(1, m))[0]
+    maxes = np.random.randint(minl, l, size= (1, t))[0]
 
-    return np.array([times[0], types[0]]), maxes[0]
+    return tuples, np.array(types), maxes
 
 # Sample input
-# print(generate_constraints(20, 8, 16, 2, 5, 4))
+times = [8.5, 9, 9.5, 10, 10.5, 11, 12.5, 1.5, 2.5, 3, 3.5, 7.5]
+print(generate_constraints(20, times, 2, 5, 4))
