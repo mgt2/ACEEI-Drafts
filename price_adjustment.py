2c4eaffcd80b650a09f7edc494510384f4739dbf
Garner Thompson <garnerthompson@dynamic-oit-vapornet100-10-9-152-191.princeton.edu>
2023-11-27 14:09:17 -0500

    float conversion

--- price_adjustment.py
+++ price_adjustment.py
@@ -59,7 +59,7 @@ def find_min_include(node, j, i, opt_without) :
     model.setObjective(gp.quicksum(x[k] * prices[k] for k in range(m)), sense=GRB.MINIMIZE)
 
     # Constraint: must have greater preference than maximum without course j
-    model.addConstr(gp.quicksum(x[k] * data['valuations'][i][k] for k in range(m)) + gp.quicksum((l < k) * x[l] * x[k] * data['etas'][i][l][k] for l in range(m) for k in range(m)) >= opt_without + 1e-10)
+    model.addConstr(gp.quicksum(x[k] * data['valuations'][i][k] for k in range(m)) + gp.quicksum((l < k) * x[l] * x[k] * data['etas'][i][l][k] for l in range(m) for k in range(m)) >= float(opt_without) + 1e-10)
     
     # Constraint: Courses must satisfy time and type constraints
     # Type constraints must be satisfied
